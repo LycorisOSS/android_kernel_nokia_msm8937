4f6d1f8a36f961b057bf8758cbe018a83c9dbaed
Michael Niewöhner <linux@mniewoehner.de>
2018-11-25 17:57:33 +0100

    usb: core: quirks: add RESET_RESUME quirk for Cherry G230 Stream series
    commit effd14f66cc1ef6701a19c5a56e39c35f4d395a5 upstream.
    
    Cherry G230 Stream 2.0 (G85-231) and 3.0 (G85-232) need this quirk to
    function correctly. This fixes a but where double pressing numlock locks
    up the device completely with need to replug the keyboard.
    
    Signed-off-by: Michael Niewöhner <linux@mniewoehner.de>
    Tested-by: Michael Niewöhner <linux@mniewoehner.de>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#2 SMP PREEMPT Wed Mar 30 15:51:31 +07 2022"
-#define LINUX_COMPILE_BY "log1cswithwsl"
-#define LINUX_COMPILE_HOST "DESKTOP-2PQJRKK"
-#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
