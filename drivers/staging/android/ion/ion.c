5f52a656d3b34aaa0f89ba03a70857de25b0bf3c
Log1cs <tuan73176@gmail.com>
2022-03-30 18:51:30 +0700

    Simple implement of ion_handle_get_by_id

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -418,6 +418,21 @@ static struct ion_handle* ion_handle_get_check_overflow(
 	return handle;
 }
 
+struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
+						int id)
+{
+	struct ion_handle *handle;
+
+	mutex_lock(&client->lock);
+	handle = idr_find(&client->idr, id);
+	if (handle)
+		ion_handle_get(handle);
+	mutex_unlock(&client->lock);
+
+	return handle ? handle : ERR_PTR(-EINVAL);
+}
+
+
 static int ion_handle_put_nolock(struct ion_handle *handle)
 {
 	int ret = 0;
